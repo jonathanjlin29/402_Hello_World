0fd07427779e9c12036821a4abe6bd7fc9221635
Jonathan Lin <jonathanjlin29@gmail.com>
2015-10-22 13:40:33 -0700

    Hello World Changed

--- HelloWorld/src/HelloWorld.cpp
+++ HelloWorld/src/HelloWorld.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 int main(int argc , char *argv[]) {
-	cout << "Jonathan Says Jenkins is Cool!!" << endl;
+	cout << "Jonathan Says Jenkins is Cool and learning is great!!!" << endl;
 
 	return 0;
 }
